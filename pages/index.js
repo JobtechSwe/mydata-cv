3a0476af5453e66e2725467fb40be5aa2c70662c
Einar Persson <me@einarpersson.com>
2018-12-04 13:42:16 +0100

    Change background on landing page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Typography, Button, theme } from '@smooth-ui/core-sc'
+import { Box, Typography, Button, theme, styled } from '@smooth-ui/core-sc'
 import { createClient } from '@mydata/client'
 import getConfig from 'next/config'
 
@@ -8,26 +8,36 @@ const { publicRuntimeConfig: { redirectUri, clientId, operatorUrl } } = getConfi
 const client = createClient({ clientId, redirectUri, operatorUrl })
 const loginUrl = client.getLoginUrl(redirectUri)
 
+const Background = styled.div`
+  background-image: url('/static/landing-page.jpg');
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+  position: absolute;
+  top: 0;
+  z-index: -10;
+`
+
 export default () => {
   return (
     <React.Fragment>
-      <div style={{ backgroundImage: "url('/static/photo.jpg')", height: 100 + 'vh', position: 'absolute', top: 0, width: 100 + 'vw', zIndex: -10 }} />
+      <Background />
       <Box
         ml={100 + 'px'}
         mr="auto" maxWidth={550}
         style={{ backgroundColor: 'white', boxShadow: '3px 3px 7px 7px rgba(0,0,0,0.1)', padding: 50 + 'px', marginTop: 50 + 'px', borderRadius: 2 + 'px' }}>
         <Typography variant="h1">
-          C:V - Manage your <span style={{ color: theme.brick }}>Curriculum Vitae</span> online
+          C:V - Create your digital <span style={{ color: theme.brick }}>Curriculum Vitae</span>
         </Typography>
-        <p>Integrates with MyData - you have complete control</p>
+        <p>Integrates with MyData - you are in complete control</p>
         <ul style={{ listStyleType: 'circle' }}>
           <li>Create, view and edit your CV in a convenient editor</li>
+          <li>Build upon other existing CV data</li>
           <li>Choose among dozens of themes and layouts</li>
-          <li>Build upon other existing data sources</li>
           <li>Share your CV with whoever you want</li>
           <li>And more</li>
         </ul>
-        <Button variant="success" onClick={() => window.location.assign(loginUrl)}>Log in with Mydata</Button>
+        <Button variant="success" onClick={() => window.location.assign(loginUrl)} style={{ marginTop: 10 + 'px' }}>Log in with MyData</Button>
       </Box>
     </React.Fragment>
   )
