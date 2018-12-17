a348496bdf4a4a05efb62aedf974e40d7816871b
Johan Öbrink <johan.obrink@gmail.com>
2018-12-17 13:26:22 +0100

    Added QR Code component

--- pages/auth.js
+++ pages/auth.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Box, Typography } from '@smooth-ui/core-sc'
 import axios from 'axios'
+import QRCode from 'qrcode.react'
 
 export default () => {
   const [id, setId] = useState(null)
@@ -22,10 +23,11 @@ export default () => {
       height="100vh"
       width="100vw"
     >
-      <Box textAlign="center">
-        {id && <Typography variant="h6">Enter the code for this consent request:</Typography>}
+      {id && <Box textAlign="center">
+        <Typography variant="h6">Enter the code for this consent request:</Typography>
         <Typography variant="h1">{id}</Typography>
-      </Box>
+        <QRCode value={id} />
+      </Box>}
     </Box>
   )
 }
