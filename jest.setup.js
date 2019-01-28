84ee6b88a0e42395aa5487f2c8db2466aafcbb27
Einar Persson <me@einarpersson.com>
2019-01-28 11:39:40 +0100

    Remove more old code

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,4 @@
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-jest.mock('next/config', () => () => ({
-  publicRuntimeConfig: {
-    operatorUrl: 'aTotallyLegitOperatorUrl'
-  }
-}))
 
 configure({ adapter: new Adapter() })
